d010c0be70381626750a81e1d5bcd7f457da2c52
jeremy <jeremy@jeremy.local>
2017-09-28 08:16:53 +0200

    Starter

---
+++ step_5/monSite/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Green</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+   <?php echo "<h1>My second version</h1>"; ?>
+</body>
+</html>
